9006538fa22df0b1dcce86d31e35f58ea968db47
narjoDev <63086420+narjoDev@users.noreply.github.com>
2024-11-30 21:36:49 -0800

    day-01 cleanup (naming, comments, spacing)

--- day-01/solution.js
+++ day-01/solution.js
@@ -1,49 +1,53 @@
 function parseToLists(file) {
-  // get left and right lists
   let lines = file.split("\n");
-  const left = [];
-  const right = [];
+  const leftList = [];
+  const rightList = [];
+
   lines.forEach((line) => {
-    let [leftNumber, rightNumber] = line.split(/\s+/).map((n) => Number(n));
-    left.push(leftNumber);
-    right.push(rightNumber);
+    let [left, right] = line.split(/\s+/).map((str) => Number(str));
+    leftList.push(left);
+    rightList.push(right);
   });
-  return [left, right];
+
+  return [leftList, rightList];
 }
 
 function partOne(file) {
   const [left, right] = parseToLists(file);
-  //sort both lists to par up
+
+  //sort both lists to pair up
   left.sort();
   right.sort();
-  //convert each index to distance between the two(abs)
+
+  //convert each index to distance between the two (abs)
+  //sum all distances
   let totalDistance = 0;
   for (let index = 0; index < left.length; index += 1) {
     totalDistance += Math.abs(left[index] - right[index]);
   }
-  //some all distances
+
   return totalDistance;
-  return;
 }
 
-function listToTally(array) {
-  const count = {};
+function tally(array) {
+  const tallies = {};
   array.forEach((value) => {
-    count[value] = (count[value] ?? 0) + 1;
+    tallies[value] = (tallies[value] ?? 0) + 1;
   });
-  return count;
+
+  return tallies;
 }
 
 function partTwo(file) {
   const [left, right] = parseToLists(file);
-  const rightTally = listToTally(right);
+  const rightTally = tally(right);
 
-  let total = 0;
-  left.forEach((v) => {
-    total += v * (rightTally[v] ?? 0);
+  let totalSimilarity = 0;
+  left.forEach((number) => {
+    totalSimilarity += number * (rightTally[number] ?? 0);
   });
 
-  return total;
+  return totalSimilarity;
 }
 
 module.exports = { partOne, partTwo };
