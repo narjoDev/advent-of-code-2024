722ab7259eb9797592802fa1a2ba50e52bf79cf4
narjoDev <63086420+narjoDev@users.noreply.github.com>
2024-12-02 12:04:50 -0800

    [template] solution clarifying comments and default line splitter

--- day-00/solution.js
+++ day-00/solution.js
@@ -1,9 +1,21 @@
-function partOne(file) {
+function splitToLines(input) {
+  return input.split("\n");
+}
+
+function partOne(input) {
+  const lines = splitToLines(input);
   return;
 }
 
-function partTwo(file) {
+function partTwo(input) {
+  const lines = splitToLines(input);
   return;
 }
 
+/*
+export to wrapper file that:
+- reads input from text file (using fs.readFileSync)
+- passes that raw string as argument to partOne & partTwo
+- outputs return values and tests against known answers
+*/
 module.exports = { partOne, partTwo };
