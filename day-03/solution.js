f110bbd78f309f0b4b9fe0b0dd32c6856a13fa20
narjoDev <63086420+narjoDev@users.noreply.github.com>
2024-12-02 21:49:23 -0800

    day-03 clarity revisions

--- day-03/solution.js
+++ day-03/solution.js
@@ -10,34 +10,36 @@ function evaluateMultiplication(instruction) {
 function partOne(input) {
   //scan match valid sections
   const uncorrupted = input.match(/mul\(\d+,\d+\)/g);
+
   //evaluate valid things
-  const results = uncorrupted.map((instruction) =>
+  const products = uncorrupted.map((instruction) =>
     evaluateMultiplication(instruction)
   );
+
   //add up
-  return sum(results);
+  return sum(products);
 }
 
 function partTwo(input) {
   // ignore instructions that have don't() after the most recent do()
   // split on do() then split those on don't
   const doSegments = input.split("do()");
+
   const enabledSegments = doSegments.map((segment) => {
     const split = segment.split("don't()");
-    return split[0];
+    return split[0]; // first element is before any "don't()"
   });
 
   let operations = [];
-
   enabledSegments.forEach((segment) => {
     operations = operations.concat(segment.match(/mul\(\d+,\d+\)/g));
   });
 
-  const results = operations.map((instruction) =>
+  const products = operations.map((instruction) =>
     evaluateMultiplication(instruction)
   );
 
-  return sum(results);
+  return sum(products);
 }
 
 /*
